a7ad533e04dbba38abd2a804474b9def006214ca
mohai <mohai.cse@gmail.com>
2024-05-20 15:19:25 +0600

    Assignment04 is submitted.

---
+++ src/Assignment04/MyPack02/AccModifierTest04.java
@@ -0,0 +1,17 @@
+package Assignment04.MyPack02;
+
+import Assignment04.MyPack01.AccModifierTest01;
+
+public class AccModifierTest04 {
+
+    public static void main(String[] args) {
+
+        AccModifierTest01 obj = new AccModifierTest01();
+
+        obj.name("Jannat"); //public method
+//        obj.phone(555666777); //private method
+//        obj.address("Rajshahi"); //default method
+//        obj.email("jannat@email.com"); //protected method
+
+    }
+}
