07b985155b91e91fc060169bff1e697ed8c06b07
mohai <mohai.cse@gmail.com>
2024-05-20 15:23:14 +0600

    Assignment05 is submitted.

---
+++ src/Assignment05/NamingConvention.java
@@ -0,0 +1,19 @@
+package Assignment05;
+
+public class NamingConvention {
+    public static void main(String[] args){
+
+        NamingConvention object01 = new NamingConvention();
+        object01.displayName("Md. Mohai Minul Islam");
+    }
+
+    public void displayName(String name){
+
+        int MAX_AGE = 25;
+
+        System.out.println(name);
+        System.out.println(MAX_AGE);
+
+
+    }
+}
